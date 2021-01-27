405d6bb78452cd4117665c48792271dcf9d6fe1d
saorim10 <marcelo.saorim@gmail.com>
2021-01-27 16:18:14 -0300

    projeto terminado

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Sistema de Contas Celebrai">
+        <div className="display-4">
+            Bem Vindo!!!
+        </div>
+        <hr />
+        <p className="mb-0">
+            Sistema para Contabilidade das Igrejas do 
+            Ministério Celebrai.
+        </p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+import React from 'react';
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Marcelo <span className="text-danger">Saorim</span></strong>
+        </span>
+    </footer>
